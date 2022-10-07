e0bef46aa532b5bedd60e3865193cbe71e66efc8
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-07 09:24:31 +0200

    Initial Commit

---
+++ NoteDictate Watch App/NoteDictateApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteDictateApp.swift
+//  NoteDictate Watch App
+//
+//  Created by Zaid Neurothrone on 2022-10-07.
+//
+
+import SwiftUI
+
+@main
+struct NoteDictate_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
